b40fd460c0a5abfc4c5c3d44e9029589e6fa20b6
Vitaly Brandes <vitalyb@dip-tech.com>
2023-01-15 14:41:50 +0200

    Added Dynamic Breadcrumb ViewCompoent that building breadcrumb based on Route
    Added Footer View Componet

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ViewComponentsDemo
 .net ViewComponents Demo collection
+
+
+Added Dynamic Breadcrumb ViewCompoent that building breadcrumb based on Route
+Dynamic Breadcrumb ViewCompoent allow to present Breadcrumb on Pares or Views including Hiperlink to previous page.
+
+
+Added Footer View Componet
+Present signature + cureent year.
---
+++ ViewComponentsCollection/ViewComponents/Breadcrumbs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace RazorPages.ViewComponents
+{
+    public class BreadcrumbsViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var myRoute = Url.RouteUrl(RouteData.Values);
+
+            List<string> items = new List<string>();
+
+            string[] words = myRoute.Split('/',StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                items.Add(word);
+   
+            }
+            return await Task.FromResult((IViewComponentResult)View(items));
+        }
+    }
+}
